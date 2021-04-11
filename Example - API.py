cafaa7c626a3793688d00540480122b18e196249
schmokel <rasmusschmokel@gmail.com>
2021-04-11 11:15:57 +0200

    Example file restored

--- Example - API.py
+++ Example - API.py
@@ -4,14 +4,15 @@
 from FBAdLibrarian import api
 import pandas as pd
 
-ads = api.AdLib("Insert-access-token")
+ads = api.AdLib("INSERT-ACCESS-TOKEN",
+                version='v10.0')
 
 
 #add parameters for your search
 # For available parameters, visit https://developers.facebook.com/docs/marketing-api/reference/ads_archive/v9.0
-ads.add_parameters(ad_delivery_date_min = "2020-10-01", ad_delivery_date_max = "2020-10-01", ad_type = "POLITICAL_AND_ISSUE_ADS",
+ads.add_parameters(ad_delivery_date_min = "2021-01-01", ad_delivery_date_max = "2021-01-05", ad_type = "POLITICAL_AND_ISSUE_ADS",
                    ad_reached_countries = 'US', ad_active_status = "ALL", impression_condition = 'HAS_IMPRESSIONS_LAST_90_DAYS',
-                   search_terms = "healthcare")
+                   search_terms = "Biden")
 
 
 # get the data
